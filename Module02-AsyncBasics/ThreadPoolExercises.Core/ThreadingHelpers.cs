1b711c8b15df70dc6661fa9a15eeccfe96dc9a4a
Yogi Ramchandani <yogiramchandani@gmail.com>
2020-11-21 22:05:47 +0000

    Implementation for Module 2

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -11,9 +11,12 @@ namespace ThreadPoolExercises.Core
             //   HINT: you may use `Join` to wait until created Thread finishes
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
-
-
-
+            Thread thread = new Thread(() => Start(action, repeats, token, errorAction))
+            {
+                IsBackground = true
+            };
+            thread.Start();
+            thread.Join();
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
@@ -23,8 +26,31 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            using (AutoResetEvent evt = new AutoResetEvent(false))
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    Start(action, repeats, token, errorAction);
+                    evt.Set();
+                });
+                evt.WaitOne();
+            }
+        }
 
-
+        private static void Start(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
+        {
+            try
+            {
+                for (int i = 0; i < repeats; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    action();
+                }
+            }
+            catch (Exception e)
+            {
+                errorAction?.Invoke(e);
+            }
         }
     }
 }
